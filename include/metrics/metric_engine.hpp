30214aca2b739d0be026fed71952227a2d023574
Have A Nice Day <ducminhtong2k3@gmail.com>
2026-07-15 14:33:38 +0700

    feat: add MetricEngine

---
+++ include/metrics/metric_engine.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <chrono>
+
+#include "metrics/symbol_store.hpp"
+#include "metrics/types.hpp"
+
+namespace metrics {
+
+class MetricEngine {
+public:
+    explicit MetricEngine(int64_t window_ms = 60'000) : store_(window_ms) {}
+
+    void Push(const Tick& tick) {
+        store_.Push(tick.symbol, tick.timestamp * 1000, tick.price, tick.volume);
+    }
+
+    Metrics Query(const std::string& symbol) {
+        return store_.Query(symbol, NowMillis());
+    }
+
+private:
+    static int64_t NowMillis() {
+        return std::chrono::duration_cast<std::chrono::milliseconds>(
+                   std::chrono::system_clock::now().time_since_epoch())
+            .count();
+    }
+
+    SymbolStore store_;
+};
+
+}
